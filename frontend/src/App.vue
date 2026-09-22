303403fed065db8cdc3bf63e7b35c5ee14854e50
Jesper Eriksson Stenberg <jespermail@gmail.com>
2026-09-22 20:06:06 +0200

    App.vue structure

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,9 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <div id="app">
+    <RouterView />
+  </div>
 </template>
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
+<style scoped></style>
